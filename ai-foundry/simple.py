261495b418d8c44c039649ddf6ec4354ca6c0989
KARTHIKAKRISHNA123 <athvikaalm2018@gmail.com>
2026-04-23 21:01:41 +0530

    feat: Learnt and Implemented a project with Microsoft AI Foundry

---
+++ ai-foundry/simple.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+
+endpoint = "https://ai-foundry-new-learning-by-kk1234567890.services.ai.azure.com/api/projects/proj-default/openai/v1"
+key = "8XvEbONp1wYYNQyHkipmMGSbEnczTfefizABLHyFU0EmPwMJ98jxJQQJ99CDACqBBLyXJ3w3AAAAACOGV4DU"
+
+deployment_name = "gpt-4.1-nano-1"
+
+client = OpenAI(
+    base_url=endpoint,
+    api_key=key,
+)
+
+
+response = client.chat.completions.create(
+    model=deployment_name,
+    messages=[
+        {"role": "system", "content": "You are a code expert."},
+        {"role": "user", "content": "give me the optimised code for bubble sort algorithm in python."},
+    ],
+
+)
+
+
+print("Response from Azure AI Foundry:")
+print(response.choices[0].message.content)
